465d037813875d1d0abc004bc2d13a4e81fd570c
First Eduspec Inc. - Learning Innovations Unit <144110944+fei-learninginnovationsunit@users.noreply.github.com>
2023-11-24 14:45:47 +0800

    Created mrc-sample-a9g-code.ino

---
+++ sample-a9g-code.ino
@@ -0,0 +1,57 @@
+#include <SoftwareSerial.h>
+
+// Define the RX and TX pins for SoftwareSerial communication
+const int A9G_RX = 2; // Connect A9G TX to Arduino digital pin 2
+const int A9G_TX = 3; // Connect A9G RX to Arduino digital pin 3
+
+SoftwareSerial A9GSerial(A9G_RX, A9G_TX);
+
+void setup() {
+  // Start communication with Arduino IDE Serial Monitor
+  Serial.begin(9600);
+  while (!Serial);
+
+  // Start communication with A9G module
+  A9GSerial.begin(115200);
+
+  // Wait for the A9G module to initialize
+  delay(1000);
+  
+  Serial.println("A9G SMS Sender Initialized");
+}
+
+void loop() {
+  // Check if there is data available from the Arduino IDE Serial Monitor
+  if (Serial.available()) {
+    // Read the command from the Serial Monitor
+    String command = Serial.readStringUntil('\n');
+
+    // Check if the command is not empty
+    if (command.length() > 0) {
+      // Send SMS using the A9G module
+      sendSMS("+639452773486", command); // Replace with the desired phone number
+
+      // Print a message to the Arduino IDE Serial Monitor
+      Serial.println("SMS Sent!");
+    }
+  }
+}
+
+// Function to send an SMS using A9G module
+void sendSMS(String phoneNumber, String message) {
+  // Set SMS text mode
+  A9GSerial.println("AT+CMGF=1");
+  delay(1000);
+
+  // Specify the phone number
+  A9GSerial.print("AT+CMGS=\"");
+  A9GSerial.print(phoneNumber);
+  A9GSerial.println("\"");
+  delay(1000);
+
+  // Send the SMS message
+  A9GSerial.print(message);
+  delay(100);
+  A9GSerial.write(26); // ASCII code for Ctrl+Z to send the SMS
+  delay(1000);
+}
